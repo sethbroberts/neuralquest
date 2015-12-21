64f3f8223ba3dae774ab17978a57a5a11e83e0d9
Leslie Pajuelo <l.l.pajuelo@gmail.com>
2015-12-21 09:55:20 -0800

    Finish merge

--- test/spec/client/auth.controller.spec.js
+++ test/spec/client/auth.controller.spec.js
@@ -1,39 +1,47 @@
+/*=====================================================
+=            unit test for app/auth/auth.controller.js            =
+=====================================================*/
 
-
-
-
-
-
-  
-  
-      expect(typeof AuthCtrl.login).toBe("function");
-      expect(typeof AuthCtrl.loginBtnToggle).toBe("function");
-      expect(typeof AuthCtrl.loginWithFacebook).toBe("function");
-      expect(typeof AuthCtrl.loginWithGoogle).toBe("function");
-      expect(typeof AuthCtrl.register).toBe("function");
-      expect(typeof AuthCtrl.resetPwd).toBe("function");
-    $controller = _$controller_;
-    //come up with methods unit test here
-    AuthCtrl = $controller('AuthCtrl', {$scope: $scope});
-    it('should have login Method', function () {
-    it('should have loginBtnToggle Method', function () {
-    it('should have loginWithFacebook Method', function () {
-    it('should have loginWithGoogle Method', function () {
-    it('should have register Method', function () {
-    it('should have resetPwd Method', function () {
-    var $scope = {};
-    });
-    });
-    });
-    });
-    });
-    });
-  beforeEach(inject(function (_$controller_){
-  beforeEach(module('neuralquestApp'));
-  describe('Authctrl methods', function () {
-  describe('method unit tests', function () {
+describe('auth.controller.spec', function () {
   var $controller, AuthCtrl;
+  
+  beforeEach(module('neuralquestApp'));
+
+  beforeEach(inject(function (_$controller_){
+    var $scope = {};
+    $controller = _$controller_;
+    AuthCtrl = $controller('AuthCtrl', {$scope: $scope});
   }));
+
+  describe('Authctrl methods', function () {
+    it('should have login Method', function () {
+      expect(typeof AuthCtrl.login).toBe("function");
+    });
+    it('should have register Method', function () {
+      expect(typeof AuthCtrl.register).toBe("function");
+    });
+    it('should have loginWithFacebook Method', function () {
+      expect(typeof AuthCtrl.loginWithFacebook).toBe("function");
+    });
+    it('should have loginWithGoogle Method', function () {
+      expect(typeof AuthCtrl.loginWithGoogle).toBe("function");
+    });
+    it('should have loginBtnToggle Method', function () {
+      expect(typeof AuthCtrl.loginBtnToggle).toBe("function");
+    });
+    it('should have resetPwd Method', function () {
+      expect(typeof AuthCtrl.resetPwd).toBe("function");
+    });
   });
+<<<<<<< 18bd33da74eee07c1794869618741b27db241b4f
   
 
+=======
+
+  describe('method unit tests', function () {
+    //come up with methods unit test here
+  });
+
+  
+});
+>>>>>>> Finish merge
